75a268c5fff2f072487ebf16935329950cdedf40
The-Herald-Sun <samuelryanmason@gmail.com>
2025-06-09 17:14:28 +1000

    Method outlines

--- gallery/resources/image_resource.py
+++ gallery/resources/image_resource.py
@@ -3,14 +3,14 @@ from falcon import MEDIA_JSON, HTTP_200, HTTP_201
 
 class ImageResource:
     def __init__(self, dal) -> None:
-        self.dal = dal
+        self.dal = dal()
 
     def on_get(self, req, resp):
-        artifacts = self.dal.get_all_artifacts()
+        images = self.dal.get_all_images()
 
         resp.content_type = MEDIA_JSON
         resp.status = HTTP_200
-        resp.media = artifacts
+        resp.media = images
 
     def on_post(self, req, resp):
         newArtifact = req.get_media()
@@ -19,7 +19,23 @@ class ImageResource:
         createdArtifact = self.dal.add_new_artifact(newArtifact)
         
         resp.content_type = MEDIA_JSON
-        resp.status = HTTP_201
+        resp.status = HTTP_200
         resp.media = createdArtifact
         resp.location = '/api/artifacts/' + createdArtifact["id"]
+
+    def on_get_item(self, req, resp, id):
+        image = self.dal.get_image(id)
+
+        resp.content_type = MEDIA_JSON
+        resp.status = HTTP_200
+        resp.media = image
+
+    def on_update_item(self, req, resp, id):
+        resp.status = HTTP_201
+        self.dal.update_image(req.get_media())
+
+    def on_delete_item(self, req, resp, id):
+        resp.status = HTTP_201
+
+        self.dal.delete_image(id)
         
